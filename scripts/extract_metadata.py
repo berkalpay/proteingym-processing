98b3230a070635cbb38e0bba4c2e5a2285f92eb9
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-07-28 10:40:07 -0400

    add metadata extraction script

---
+++ scripts/extract_metadata.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+metadata_filename = sys.argv[1]
+dms_id = sys.argv[2]
+extracted_filename = sys.argv[3]
+
+metadata = pd.read_csv(metadata_filename)
+extracted = metadata.loc[metadata["DMS_id"] == "dms_id"]
+if len(extracted) == 0:
+    raise ValueError("DMS ID {dms_id} isn't in the given metadata file")
+elif len(extracted) > 1:
+    raise ValueError("DMS ID {dms_id} is in the given metadata file multiple times")
+extracted.to_csv(extracted_filename, index=False)
