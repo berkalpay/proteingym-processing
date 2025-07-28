7ec1a67e1322d746a8e566de27e4c039c7874a79
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-07-28 14:30:19 -0400

    metadata extraction: fix filter, rename variables

--- scripts/extract_metadata.py
+++ scripts/extract_metadata.py
@@ -1,14 +1,14 @@
 import sys
 import pandas as pd
 
-metadata_filename = sys.argv[1]
+metadata_path = sys.argv[1]
 dms_id = sys.argv[2]
-extracted_filename = sys.argv[3]
+extracted_metadata_path = sys.argv[3]
 
-metadata = pd.read_csv(metadata_filename)
-extracted = metadata.loc[metadata["DMS_id"] == "dms_id"]
+metadata = pd.read_csv(metadata_path)
+extracted = metadata.loc[metadata["DMS_id"] == dms_id]
 if len(extracted) == 0:
     raise ValueError("DMS ID {dms_id} isn't in the given metadata file")
 elif len(extracted) > 1:
     raise ValueError("DMS ID {dms_id} is in the given metadata file multiple times")
-extracted.to_csv(extracted_filename, index=False)
+extracted.to_csv(extracted_metadata_path, index=False)
