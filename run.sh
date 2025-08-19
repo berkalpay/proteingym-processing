b2225dee3036f3daaaddd8a7e753584a779e7db7
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-08-19 13:15:46 -0400

    migrate from cluster config to profile

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 python3 scripts/extract_metadata.py data/DMS_substitutions_internal.csv data/metadata/
 
-snakemake -n -p --use-conda \
-  -j 100 \
-  --cluster-config cluster-config.json \
-  --cluster "sbatch --mem={cluster.mem} -t {cluster.time} -c {cluster.cores} -p {cluster.partition} -G {cluster.gpu} --output=logs-jobs/slurm-%j.out" \
-  --keep-going \
-  --latency-wait 60 \
-  --restart-times 0
+snakemake -n -p --use-conda --profile slurm
