9d213ce3313d283e23a60d674ae75dc51ba87414
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-08-19 13:00:52 -0400

    fix: extract metadata paths

--- scripts/extract_metadata.py
+++ scripts/extract_metadata.py
@@ -3,13 +3,14 @@ import sys
 import pandas as pd
 
 metadata_path = sys.argv[1]
-extracted_metadata_path = sys.argv[2]
+extracted_metadata_dir = sys.argv[2]
 
 metadata = pd.read_csv(metadata_path)
 for dms_id in metadata["DMS_id"]:
     extracted = metadata.loc[metadata["DMS_id"] == dms_id]
+    extracted_metadata_path = os.path.join(extracted_metadata_dir, f"{dms_id}.csv")
     if not (
         os.path.exists(extracted_metadata_path)
         and pd.read_csv(extracted_metadata_path) == extracted
     ):
-        extracted.to_csv(extracted_metadata_path + f"{dms_id}.csv", index=False)
+        extracted.to_csv(extracted_metadata_path, index=False)
