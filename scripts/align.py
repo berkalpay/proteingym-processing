45af5b7601e2ab1764720f8832753694321816c0
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-10-31 14:53:23 -0400

    more 8dbfbc42a2383308b4665f58c631eee3a8b5b216

--- scripts/align.py
+++ scripts/align.py
@@ -15,12 +15,11 @@ target_seq = metadata.at[0, "target_aa_seq"]
 # Run alignment
 theta = 1 - (0.01 if virus else 0.2)
 os.makedirs("data/temp/", exist_ok=True)
-target_seq_path = f"data/temp/{dms_id}"
+target_seq_path = f"data/temp/{dms_id}.fa"
 with open(target_seq_path, "w") as f:
-    f.write(">{dms_id}\n")
+    f.write(f">{dms_id}\n")
     f.write(target_seq)
 job_name_prefix = f"data/EVcouplings/{dms_id}/{bitscore}"
 os.system(
-    f"evcouplings --protein {dms_id} -b {bitscore} -s {target_seq_path}"
-    f"--theta {theta} --colcov 70 --prefix {job_name_prefix} configs/align.txt --yolo"
+    f"evcouplings --protein {dms_id} -b {bitscore} -s {target_seq_path} --theta {theta} --colcov 70 --prefix {job_name_prefix} configs/align.txt --yolo"
 )  # TODO: use --stages arg instead of separate configs?
